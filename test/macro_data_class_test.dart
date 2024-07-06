1011cc425b09320bfcf5b9ccbd8830af2fda2fbd
Pedro Nishiyama <nishiyama.v3@gmail.com>
2024-07-06 18:55:28 -0300

    DataForm - add tests

--- test/macro_data_class_test.dart
+++ test/macro_data_class_test.dart
@@ -3,10 +3,41 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:macro_data_class/macro_data_class.dart';
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
+  group('Test macro', () {
+    test('get values', () {
+      final macroTest = MacroTest('test', 50);
+
+      expect(macroTest.get('name'), 'test');
+      expect(macroTest.get('age'), 50);
+    });
+
+    test('set values', () {
+      final macroTest = MacroTest('test', 50);
+
+      macroTest.set('name', 'macro');
+      macroTest.set('age', 100);
+
+      expect(macroTest.get('name'), 'macro');
+      expect(macroTest.get('age'), 100);
+    });
+
+    test('getter/setter', () {
+      final macroTest = MacroTest('test', 50);
+
+      macroTest.set('ageMultiply', 2);
+
+      expect(macroTest.get('nameAge'), 'test is 100');
+    });
   });
+}
+
+@DataClassMacro()
+class MacroTest {
+  String name;
+  int age;
+
+  String get nameAge => '$name is $age';
+  set ageMultiply(int value) => age *= value;
+
+  MacroTest(this.name, this.age);
 }
